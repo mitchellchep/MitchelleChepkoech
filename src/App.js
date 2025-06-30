620779cc25382b6ce3254cb05276b8597ced2e80
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-30 13:42:40 +0300

    Styling for my About page
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import './App.css';
 import Body from './components/Body';
+import About from './components/About';
 
 const App = () => {
   return (
     <div id="app" className="App">
       <Header />
       <Body/>
+      <About/>
     </div>
   );
 }
