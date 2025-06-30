8fa47249a31e448bba67be91d1dcc5a025153cba
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-30 15:17:46 +0300

    Styling for the Projects component
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import './App.css';
 import Body from './components/Body';
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Body/>
       <About/>
+      <Projects/>
     </div>
   );
 }
