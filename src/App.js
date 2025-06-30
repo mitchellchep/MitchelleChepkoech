cf92162ad97f6966921b1ceaf3115d3e516c831a
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-30 13:28:59 +0300

    Styling for the Body
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import './App.css';
+import Body from './components/Body';
 
 const App = () => {
   return (
     <div id="app" className="App">
       <Header />
+      <Body/>
     </div>
   );
 }
