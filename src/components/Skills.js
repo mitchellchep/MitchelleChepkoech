d9b455ae3e0e1bdb01dc5449e5892ab5c89e8305
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-30 16:12:08 +0300

    Created Footer component
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,35 +1,32 @@
-// Create your Skills component here
-import {FaJs, FaReact, FaHtml5, FaCss3, FaGithub, FaDocker} from 'react-icons/fa';
+// Skills.jsx
+import { FaJs, FaReact, FaHtml5, FaCss3, FaGithub, FaDocker } from "react-icons/fa";
 
 const Skills = () => {
-    const skillsArr = [
-        { "name": "JavaScript", "icon": <FaJs /> },
-        { "name": "HTML", "icon": <FaHtml5 /> },
-        { "name": "CSS", "icon": <FaCss3 /> },
-        { "name": "Git", "icon": <FaGithub /> },
-        { "name": "ReactJs", "icon": <FaReact /> },
-        { "name": "Docker", "icon": <FaDocker /> }  
-    ];
-    return (
-        <div id="skills" className="skills">
-            <h2 className='title'>Skills</h2>
-            <div className='skills-holder'>
-            {
-                skillsArr.map((skill, index) => {
-                    const Icon = skill.icon;
-                    return (
-                        <i key={index} className="skill-cards">
-                            <Icon  className='skill-icon'/>
-                            <p className='skill'>{skill.name}</p>
-                        </i>
-                    )
-                })
-            }
+  const skillsArr = [
+    { name: "JavaScript", icon: FaJs },
+    { name: "HTML", icon: FaHtml5 },
+    { name: "CSS", icon: FaCss3 },
+    { name: "Git", icon: FaGithub },
+    { name: "ReactJs", icon: FaReact },
+    { name: "Docker", icon: FaDocker },
+  ];
+
+  return (
+    <div id="skills" className="skills">
+      <h2 className="title">Skills</h2>
+      <div className="skills-holder">
+        {skillsArr.map((skill, index) => {
+          const Icon = skill.icon;
+          return (
+            <div key={index} className="skill-cards">
+              <Icon className="skill-icon" />
+              <p className="skill">{skill.name}</p>
             </div>
-        </div>
-        )
-    }
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default Skills;
-
-
-              
